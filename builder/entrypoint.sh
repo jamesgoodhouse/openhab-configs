cc7881ac4fd1e7b40d5b3757b397e879659778ed
James J. Goodhouse <j.goodhouse@gmail.com>
2020-09-15 13:15:58 -0700

    further flush out config builder script

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -2,26 +2,16 @@
 
 set -euo pipefail
 
+build_configs=false
 built_configs_path=${BUILT_CONFIGS_PATH-/built_configs}
 final_configs_path=${FINAL_CONFIGS_PATH-/final_configs}
 configs_repo_path=${CONFIGS_REPO_PATH-/configs_repo}
 configs_repo_url=${CONFIGS_REPO_URL-https://github.com/jamesgoodhouse/openhab-configs.git}
-running_flag_file="$configs_repo_path/.running"
-
-cleanup () {
-  exit_code=$?
-
-  echo
-  echo 'cleaning up'
-  if [ -f "$running_flag_file" ]; then
-    rm -f "$running_flag_file"
-  fi
-
-  exit $exit_code
-}
+config_yaml_path=${CONFIG_YAML_PATH-/yamls/config/config.yaml}
+secrets_yaml_path=${SECRETS_YAML_PATH-/yamls/secrets/secrets.yaml}
 
 clone_repo () {
-  echo 'cloning configs repo'
+  echo cloning configs repo
   echo ================================================================================
 
   git clone "$configs_repo_url" --depth 1 "$configs_repo_path"
@@ -32,58 +22,54 @@ pull_latest () {
   echo ================================================================================
 
   pushd "$configs_repo_path" > /dev/null
-  git pull
+
+  git fetch
+
+  if [ "$(git rev-parse HEAD)" != "$(git rev-parse 'master@{upstream}')" ]; then
+    git pull
+    build_configs=true
+  fi
+
   popd > /dev/null
 }
 
 build_configs () {
   echo building configs
-  mkdir -p "$built_configs_path/folder"
-  touch "$built_configs_path/folder/file"
-  # gomplate and dump to $built_configs_path
+  echo ================================================================================
 
-  # ensure all these folders exists
-  # html
-  # icons
-  # items
-  # persistence
-  # rules
-  # scripts
-  # services
-  # sitemaps
-  # sounds
-  # things
-  # transform
+  gomplate --input-dir="/$configs_repo_path/configs" \
+           --output-map="/$built_configs_path/{{ .in | strings.ReplaceAll \".tmpl\" \" \" }}" \
+           --exclude='*.yaml' \
+           --exclude='.gitignore' \
+           --datasource="configs=$config_yaml_path" \
+           --datasource="secrets=$secrets_yaml_path"
+
+  # openhab seems a little happier (in its logs at least) when all of these dirs exist
+  local default_conf_dirs=(html icons items persistence rules scripts services sitemaps sounds things transform)
+
+  for d in "${default_conf_dirs[@]}"; do
+    mkdir -p "$built_configs_path/$d"
+  done
 }
 
 copy_configs () {
   echo copying built configs to final destination
   echo ================================================================================
 
-  pushd "$built_configs_path" > /dev/null
-  rsync --verbose --checksum --recursive --delete . "$final_configs_path"
-  popd > /dev/null
+  # ensure src dir has trailing slash so we only copy its contents
+  [[ "${built_configs_path}" != */ ]] && src="${built_configs_path}/"
+
+  rsync --verbose --checksum --recursive --delete "$src" "$final_configs_path"
 }
-
-# bow out early if this is already running elsewhere (e.g., openhab init container)
-if [ -f "$running_flag_file" ]; then
-  echo 'already running elsewhere; exiting'
-  exit 0
-fi
-
-# register the cleanup function incase of unexpected error/interuption
-trap cleanup ERR INT TERM
-
-# both cronjob and init-container for openhab run this, so this should prevent them from colliding
-touch "$running_flag_file"
 
 if [ ! -d "$configs_repo_path"/.git ]; then
   clone_repo
+  build_configs=true
 else
   pull_latest
 fi
 
-build_configs
-copy_configs
-
-cleanup
+if [ "$build_configs" = true ]; then
+  build_configs
+  copy_configs
+fi
